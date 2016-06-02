ae30b280b0dc826d92a6fba98c01f22435e18a5f
paco <franmenendezpouso@gmail.com>
2016-06-02 11:13:09 +0200

    Clases creadas

---
+++ Enigma.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Enigma here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enigma
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class Enigma
+     */
+    public Enigma()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Mecanismo.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Mecanismo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mecanismo
+{
+    // instance variables - replace the example below with your own
+    private int numero;
+
+    /**
+     * Constructor for objects of class Mecanismo
+     */
+    public Mecanismo(int num)
+    {
+        // initialise instance variables
+        numero = num;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int getNumero()
+    {
+        // put your code here
+        return numero;
+    }
+    
+    public String toString(){
+        return "El numero introducido es: "+numero;
+    
+    }
+}
